482d41b3ddc4ac353aa9a96bebb9648e30227824
wraikny <writtenknown@gmail.com>
2020-08-05 19:15:06 +0900

    refine CounterSample

--- sample/Altseed2.BoxUI.Sample/CounterSample.cs
+++ sample/Altseed2.BoxUI.Sample/CounterSample.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Altseed2.BoxUI.Builtin;
@@ -7,18 +7,10 @@ namespace Altseed2.BoxUI.Sample
 {
     sealed class CounterSample : Node
     {
-        private class State
-        {
-            public int Count { get; set; }
-        }
-
-        private readonly State state_;
         private readonly BoxUIRootNode uiRoot_;
 
         public CounterSample()
         {
-            state_ = new State();
-
             var cursor = new BoxUIMouseCursor("Mouse");
             AddChildNode(cursor);
 
@@ -29,50 +21,19 @@ namespace Altseed2.BoxUI.Sample
 
         protected override void OnAdded()
         {
-            MakeView(uiRoot_, state_);
+            MakeView(uiRoot_, new State());
+        }
+
+        private class State
+        {
+            public int Count { get; set; }
         }
 
         static void MakeView(BoxUIRootNode root, State state)
         {
-            var font_ = Font.LoadDynamicFont("TestData/Font/mplus-1m-regular.ttf", 70);
+            var font = Font.LoadDynamicFont("TestData/Font/mplus-1m-regular.ttf", 70);
 
-            var textElem = TextElement.Create(color: Params.TextColor, text: $"{state.Count}", font: font_);
-
-            void Decrement(IBoxUICursor _)
-            {
-                state.Count--;
-                textElem.Node.Text = $"{state.Count}";
-                Console.WriteLine(state.Count);
-            }
-
-            void Increment(IBoxUICursor _)
-            {
-                state.Count++;
-                textElem.Node.Text = $"{state.Count}";
-                Console.WriteLine(state.Count);
-            }
-
-            Element CreateButton(Font font, string text, Action<IBoxUICursor> action)
-            {
-                var background = RectangleElement.Create(color: Params.DefaultColor);
-
-                // マージン
-                return MarginElement.Create(new Vector2F(0.05f, 0.05f), Margin.Relative)
-                    // 背景色
-                    .With(background)
-                    // 中心にテキスト
-                    .With(AlignElement.Create(Align.Center, Align.Center)
-                        .With(TextElement.Create(color: Params.TextColor, text: text, font: font))
-                    )
-                    // 当たり判定・アクション
-                    .With(ButtonElement.CreateRectangle()
-                        .OnRelease(action)
-                        .OnFree(_ => { background.Node.Color = Params.HoverColor; })
-                        .OnHold(_ => { background.Node.Color = Params.HoldColor; })
-                        .WhileNotCollided(() => { background.Node.Color = Params.DefaultColor; })
-                    )
-                ;
-            }
+            var textElem = TextElement.Create(color: Params.TextColor, text: $"{state.Count}", font: font);
 
             // クリア
             root.ClearElement();
@@ -92,13 +53,43 @@ namespace Altseed2.BoxUI.Sample
                             // X方向分割
                             .With(ColumnElement.Create(Column.X)
                                 // ボタン
-                                .With(CreateButton(font_, "-", Decrement))
-                                .With(CreateButton(font_, "+", Increment))
+                                .With(CounterButton(font, "-", _ => {
+                                    state.Count--;
+                                    textElem.Node.Text = $"{state.Count}";
+                                    Console.WriteLine(state.Count);
+                                }))
+                                .With(CounterButton(font, "+", _ => {
+                                    state.Count++;
+                                    textElem.Node.Text = $"{state.Count}";
+                                    Console.WriteLine(state.Count);
+                                }))
                             )
                         )
                     )
                 )
             );
+        }
+
+        public static Element CounterButton(Font font, string text, Action<IBoxUICursor> action)
+        {
+            var background = RectangleElement.Create(color: Params.DefaultColor);
+
+            // マージン
+            return MarginElement.Create(new Vector2F(0.05f, 0.05f), Margin.Relative)
+                // 背景色
+                .With(background)
+                // 中心にテキスト
+                .With(AlignElement.Create(Align.Center, Align.Center)
+                    .With(TextElement.Create(color: Params.TextColor, text: text, font: font))
+                )
+                // 当たり判定・アクション
+                .With(ButtonElement.CreateRectangle()
+                    .OnRelease(action)
+                    .OnFree(_ => { background.Node.Color = Params.HoverColor; })
+                    .OnHold(_ => { background.Node.Color = Params.HoldColor; })
+                    .WhileNotCollided(() => { background.Node.Color = Params.DefaultColor; })
+                )
+            ;
         }
     }
 }
